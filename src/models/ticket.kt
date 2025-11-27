fd165d9ab4caa3e3e4dd0a2be04b7565d3e6a676
valeriu294 <0comav27@solent.ac.uk>
2025-11-27 18:30:50 +0000

    add ticket class

--- src/models/ticket.kt
+++ src/models/ticket.kt
@@ -1,2 +1,13 @@
 package models
 
+import java.time.LocalDateTime
+
+class Ticket (
+    var origin:String = "" ,
+    var destination:String = "" ,
+    var ticketType:String = "" ,
+    var price : Double = 0.0 ,
+    val purchaseDateTime : LocalDateTime = LocalDateTime.now(),
+    val ticketID : String = ""
+){
+}
