2bbb699fb2740a8744677cd59cc609a3bd5e9533
valeriu294 <0comav27@solent.ac.uk>
2025-12-08 11:21:17 +0000

    add special offer class

--- src/models/specialOffer.kt
+++ src/models/specialOffer.kt
@@ -1,2 +1,12 @@
 package models
+import java.time.LocalDate
 
+data class SpecialOffer(
+    val id: Int ,
+    val stationName : String,
+    val discount : Double,
+    val startDate : LocalDate ,
+    val endDate : LocalDate ,
+    val desccription : String
+
+)
